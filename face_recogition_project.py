dd7d806234b7dbdf8110c98b3c6384afd93637ab
godabharath70-sketch <godabharath2004@gmail.com>
2026-01-05 01:35:13 +0530

    Initial face recognition project

---
+++ face_recogition_project.py
@@ -0,0 +1,35 @@
+import cv2 as cc
+face_cascade = cc.CascadeClassifier(
+    cc.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+if face_cascade.empty():
+    print("Haar Cascade not loaded")
+    exit()
+cap = cc.VideoCapture(0)
+if not cap.isOpened():
+    print("Camera not accessible")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to get frame")
+        break
+
+    gray = cc.cvtColor(frame, cc.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,   
+        minNeighbors=4,
+        minSize=(60, 60)   
+    )
+
+    for (a, b, c, d) in faces:
+        cc.rectangle(frame, (a, b), (a + c, b + d), (0, 255, 0), 2)
+    cc.imshow("Face Detection", frame)
+    if cc.waitKey(1) == 27:
+        break
+
+cap.release()
+cc.destroyAllWindows()
+
